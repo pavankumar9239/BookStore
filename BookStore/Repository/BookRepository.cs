c13f20306d67193f030a19734becd8f578e8fbe1
Pavan kumar <pavankumar.andhavarapu@gmail.com>
2021-11-15 23:43:41 +0530

    Adding Controllers, models and views and enabling runtime compilation

---
+++ BookStore/Repository/BookRepository.cs
@@ -0,0 +1,37 @@
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Repository
+{
+    public class BookRepository
+    {
+        public List<BookModel> GetAllBooks()
+        {
+            return DataSource();
+        }
+
+        public BookModel GetBookById(int id)
+        {
+            return DataSource().Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        public List<BookModel> SearchBooks(string bookName, string authorName)
+        {
+            return DataSource().Where(x => x.Title == bookName || x.AuthorName == authorName).ToList();
+        }
+
+        List<BookModel> DataSource()
+        {
+            return new List<BookModel>()
+            {   
+                new BookModel(){Id=1, Title="MVC", AuthorName="APK"},
+                new BookModel(){Id=2, Title=".Net", AuthorName="Megha"},
+                new BookModel(){Id=3, Title="Java", AuthorName="Manasa"},
+                new BookModel(){Id=4, Title="PHP", AuthorName="Siddu"},
+            };
+        }
+    }
+}
--- BookStore/Startup.cs
+++ BookStore/Startup.cs
@@ -17,6 +17,7 @@ namespace BookStore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +38,7 @@ namespace BookStore
             //{
             //    await context.Response.WriteAsync("Hello2");
             //}); 
+            app.UseStaticFiles();
 
             app.UseRouting();
 
